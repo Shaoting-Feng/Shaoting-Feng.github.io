1123c9f232329216837e6c8a0aa15cccaae1f450
Shaoting-Feng <shaotingf@uchicago.edu>
2026-03-22 00:17:40 -0400

    Update projects
    Signed-off-by: Shaoting-Feng <shaotingf@uchicago.edu>

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,38 +6,12 @@ import Main from '../layouts/Main';
 import CellMine from '../components/Projects/Cell-mine';
 
 import data from '../data/projects';
-import myData from '../data/projects-mine';
-import pubData from '../data/publications';
 
 const sections = {
-  Research: () => (
-    <>
-      <div className="link-to" id="research" />
-      <h3>Publications:</h3>
-      <div className="cells-list">
-        {pubData.map((project) => (
-          <CellMine data={project} key={project.title} />
-        ))}
-      </div>
-    </>
-  ),
   'Open-Source': () => (
     <>
-      <div className="link-to" id="open-source" />
-      <h3>Open-source projects that I&apos;m maintaining:</h3>
       <div className="cells-list">
         {data.map((project) => (
-          <CellMine data={project} key={project.title} />
-        ))}
-      </div>
-    </>
-  ),
-  Projects: () => (
-    <>
-      <div className="link-to" id="projects" />
-      <h3>Past Projects:</h3>
-      <div className="cells-list">
-        {myData.map((project) => (
           <CellMine data={project} key={project.title} />
         ))}
       </div>
@@ -53,13 +27,6 @@ const Projects = () => (
           <h2>
             <Link to="/projects">Projects</Link>
           </h2>
-          <div className="link-container">
-            {Object.keys(sections).map((sec) => (
-              <h4 key={sec}>
-                <a href={`#${sec.toLowerCase()}`}>{sec}</a>
-              </h4>
-            ))}
-          </div>
         </div>
       </header>
       {Object.entries(sections).map(([name, Section]) => (
